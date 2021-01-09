54cc2f1e3340d8f4e7f59c17d4db36852c0fb02e
J-ADan <jiangfc1314@gmail.com>
2021-01-09 09:10:38 +0800

    1

--- src/router/tenant.js
+++ src/router/tenant.js
@@ -1,14 +1,14 @@
-// import HelloWorld from '@/components/HelloWorld'
+import console from '@/components/console'
 // 上面先引用vue文件，语法类似上面
 // import **** from '@/components/****'
 
 let router = [
   // 这里写路由
-  // {
-  //   path: '/account',
-  //   name: 'HelloWorld',
-  //   component: HelloWorld
-  // }
+  {
+    path: '/tenant',
+    name: 'console',
+    component: console
+  }
   // 每个路由逗号分割
 ]
 
